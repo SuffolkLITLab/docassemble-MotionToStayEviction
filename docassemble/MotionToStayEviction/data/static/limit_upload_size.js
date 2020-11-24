090620e2e1205a072bc00201503599b36bbdf957
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-11-24 11:53:05 -0500

    Quick and dirty behavior implemented. No accumulated file size.

--- docassemble/MotionToStayEviction/data/static/limit_upload_size.js
+++ docassemble/MotionToStayEviction/data/static/limit_upload_size.js
@@ -1,6 +1,7 @@
 // Limit upload size of file
 
-// TODO: How to keep track of total size of all files since files can be added on different pages.
+// TODO: Explore CustomDatatype js instead of the js in here
+// TODO: Keep track of total size of all files since files can be added on different pages.
 
 let dom_data = {};
 
@@ -20,42 +21,46 @@ let clear_error = function () {
 };   // Ends clear_error()
 
 let prevent_big_files = function ( var_data ) {
-  /* Shows an error if a upload is too big and deletes the files */
+  /* If the size of uploaded files is too big, show an
+  *    error and don't allow the user to continue. */
   let max_mb = var_data.variables.max_mb_per_file;
   let max_bytes = max_mb * 1000 * 1000;
   let files = dom_data.upload_node.files
-  console.log( var_data.variables.file_size_error_message );
 
   let total_size = 0;
   for ( let file of files ) { total_size += file.size; }
-  let is_too_big = total_size > max_bytes;
   if ( total_size > max_bytes ) {
     let $error = $(`.da-field-container-datatype-files label.da-has-error`);
     $error.css( 'display', 'inline-block' );
+    // WARNING: This message is not going to be translated currently
+    // See notes in YAML about possibly translating: var_data.variables.file_size_error_message
     $error.text( `The size of all the files together needs to be less than ${ max_mb } mb` );
-    // When the files are removed in here, the display
-    // looks the same, which is very confusing. Instead
-    // we're disabling the 'Next' button
+    // Disable continuing to next page.
     let next = document.querySelector('.da-field-buttons button[type="submit"]');
     next.disabled = true;
     
     // Listen for removal of file
     $('.fileinput-remove').on( 'click', clear_error );
     
+  // Otherwise make sure the error state stuff is removed/not present
   } else { clear_error(); }
 };  // Ends prevent_big_files()
 
+
+
 $( document ).ready(function () {
+  // When the document is ready, start listening for the relevant events
   
+  // The file input field itself changing (getting a value)
   $('.dafile').on( 'change', function (evnt) {
     dom_data.upload_node = evnt.target;
-    get_interview_variables( prevent_big_files );
+    get_interview_variables( prevent_big_files );  // A da function
   });
   
+  // The drag and drop area getting a file dropped
   $(document.body).on('drop', function (event) {
-    let files_input = document.querySelector('.dafile');
     dom_data.upload_node = document.querySelector('.dafile');
-    get_interview_variables( prevent_big_files );
+    get_interview_variables( prevent_big_files );  // A da function
   });
 
 });
